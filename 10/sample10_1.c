aba03f2bb8c347fbe3784382d659e764957fded4
DaikiKubota <d8763ddd@gmail.com>
2023-10-24 10:54:21 +0900

    何もステージせずにコミットすることはできないようだ

--- 10/sample10_1.c
+++ 10/sample10_1.c
@@ -1,18 +1,3 @@
-// #include <stdio.h>
-
-// #define NUMBER 5 /* Nの定義 */
-
-// int main(void)
-// {
-//     int data[NUMBER];
-//     int k=0;
-
-//     int arrayNumber = sizeof array /sizeof array[0];
-
-//     printf("arrayNumber; %d\n", arrayNumber);
-//     return 0;
-// }
-
 //配列宣言時の個数には定数しか使えない
 #include <stdio.h>
 
