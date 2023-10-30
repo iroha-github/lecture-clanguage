6dbd166b3f8c9cf266c61727a5b7141ef62a2744
DaikiKubota <d8763ddd@gmail.com>
2023-10-30 17:34:18 +0900

    11の途中まで

---
+++ 11/sample11_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//構造体を定義
+struct student{
+    int id;
+    char name[128];
+    double gpa;
+};
+
+int main(void) {
+    //構造体型の変数を作成
+    struct student A;
+
+    
+}
--- short_cut.sh
+++ short_cut.sh
@@ -3,8 +3,14 @@
 # URLを定義
 url1="https://drive.google.com/drive/u/1/folders/10_-noD0SkcMS--xg1Fqstjh-JMkt0bbn"
 
+# URL(scomb)を定義
+url2="https://scombz.shibaura-it.ac.jp/lms/course?idnumber=202303SU0445322001"
+
 # WindowsのデフォルトブラウザでURLを開く
 cmd.exe /C start "$url1"
+
+# WindowsのデフォルトブラウザでURLを開く
+cmd.exe /C start "$url2"
 
 # WSL内のユーザーフォルダパスを取得
 wsl_user_folder_path=$(wslpath -w "/home/iroha/clanguage")
