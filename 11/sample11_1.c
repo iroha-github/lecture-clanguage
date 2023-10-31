db43424cc44d41ee734ece16c8f28d10a487bd19
DaikiKubota <d8763ddd@gmail.com>
2023-10-31 09:03:19 +0900

    sample11_1.c更新

--- 11/sample11_1.c
+++ 11/sample11_1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 //構造体を定義
 struct student{
@@ -7,9 +8,17 @@ struct student{
     double gpa;
 };
 
-int main(void) {
-    //構造体型の変数を作成
+int main(void){
+    // 構造体型の変数を作成
     struct student A;
 
-    
+    // 構造体に値をセット
+    A.id = 23123;
+    strcpy(A.name, "T.S.");
+    A.gpa = 3.5;
+
+    printf("No. \t Name \t GPA\n");
+    printf("%d \t %s \t %.1f\n", A.id, A.name, A.gpa);
+
+    return 0;
 }
